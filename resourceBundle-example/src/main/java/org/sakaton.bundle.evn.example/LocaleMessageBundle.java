e096494f297b6c7d17349233b360e569549bc126
zhengshijun <zhengsj@akulaku.com>
2020-09-05 11:23:22 +0800

    添加注释

--- resourceBundle-example/src/main/java/org/sakaton.bundle.evn.example/LocaleMessageBundle.java
+++ resourceBundle-example/src/main/java/org/sakaton.bundle.evn.example/LocaleMessageBundle.java
@@ -13,6 +13,10 @@ import java.util.spi.ResourceBundleControlProvider;
 /**
  * @author sakaton
  * @version created on 2020/9/4.
+ *
+ * @see <a href="https://baike.baidu.com/item/%E8%AF%AD%E8%A8%80%E4%BB%A3%E7%A0%81/6594123?fr=aladdin">语言代码</a>
+ * @see <a href="http://www.rzfanyi.com/ArticleShow.asp?ArtID=969">语言代码缩写</a>
+ * @see <a href="http://www1.jctrans.com/tool/dm.htm">国家代码</a>
  */
 public class LocaleMessageBundle {
 
